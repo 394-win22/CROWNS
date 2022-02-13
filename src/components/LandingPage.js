8c15c88a9fe1d636be221aaf73f699f847bd7858
Bradley Pawlow <bradleypawlow99@gmail.com>
2022-02-13 14:47:40 -0600

    landing page structure

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,11 +1,31 @@
 import React from "react";
-
+import Stack  from '@mui/material/Stack';
+import { ListItem } from '@mui/material';
 
 const LandingPage = () => {
     console.log("navigated to landing page");
     return (
-        <div className="LandingPage">
-        empty 
+        <div className="LandingPage" style={{
+            position: 'absolute', left: '50%', top: '40%',
+            transform: 'translate(-50%, -50%)'
+        }}>
+            <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={4}
+            >
+                <div> 
+                    Item 1  //title
+                </div>
+                <div>
+                    Item 2 //logo image
+                </div>
+                <div>
+                    Item 3 // (figure out my hair type)
+                </div>
+               
+            </Stack>
         </div>
     );
   }
