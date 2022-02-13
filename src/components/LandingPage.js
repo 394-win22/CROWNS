a67848f2292268eb7b22668ed0bf25b07ca04311
Brian Gleason <51419601+BrianGleason@users.noreply.github.com>
2022-02-13 14:13:49 -0600

    Routing between pages

---
+++ src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const LandingPage = () => {
+    console.log("navigated to landing page");
+    return (
+        <div className="LandingPage">
+        empty 
+        </div>
+    );
+  }
+  
+  export default LandingPage;
