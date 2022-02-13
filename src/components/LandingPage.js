6c1a2e030def15cd36be891d78b28e6f892625b2
Brian Gleason <51419601+BrianGleason@users.noreply.github.com>
2022-02-13 15:55:31 -0600

    landing page styling

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,28 +1,47 @@
 import React from "react";
 import Stack  from '@mui/material/Stack';
-import { ListItem } from '@mui/material';
+import Button from '@mui/material/Button';
+import Logo from '../images/CROWNSLOGO.png';
+import "@fontsource/aileron";
+import "@fontsource/caveat";
 
 const LandingPage = () => {
     console.log("navigated to landing page");
+
+    const headerStyle = {
+        color: 'black',
+        fontSize: '3rem',
+        fontWeight: 'bold',
+        fontFamily: 'Aileron',
+      };
+
     return (
         <div className="LandingPage" style={{
-            position: 'absolute', left: '50%', top: '40%',
+            position: 'absolute', left: '50%', top: '45%',
             transform: 'translate(-50%, -50%)'
         }}>
             <Stack
                 direction="column"
                 justifyContent="center"
                 alignItems="center"
-                spacing={4}
+                spacing={0}
             >
-                <div> 
-                    Item 1  //title
+                <div style={headerStyle}> 
+                   CROWNS
                 </div>
                 <div>
-                    Item 2 //logo image
+                    <img style={{maxWidth: '20rem'}} src={Logo} alt=''/>
                 </div>
+                <div style={{fontFamily: 'Caveat', fontSize: '1.5rem'}}>
+                    Hey Girl! Are you ready to embrace <br></br>your hair and take back control?
+                </div>
+
                 <div>
-                    Item 3 // (figure out my hair type)
+                <Button variant="contained" size="large" defaultValue={30} sx={{width: 300,color: 'white', backgroundColor: "#F1CCD3", fontFamily: 'Aileron'}}> 
+                    Find out my hair type! 
+                </Button>
+
+
                 </div>
                
             </Stack>
