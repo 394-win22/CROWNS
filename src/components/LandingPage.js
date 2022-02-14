f87520fdd5a3d800548900b908fb870695a576bf
Brian Gleason <51419601+BrianGleason@users.noreply.github.com>
2022-02-14 15:59:37 -0600

    button color on hover fixed, link added to quiz page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,11 +2,26 @@ import React from "react";
 import Stack  from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Logo from '../images/CROWNSLOGO.png';
+import {Link} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "@fontsource/aileron";
 import "@fontsource/caveat";
 
 const LandingPage = () => {
     console.log("navigated to landing page");
+    let navigate = useNavigate(); 
+
+
+    const buttonStyle = {
+        width: 300,
+        color: 'white', 
+        backgroundColor: "#F1CCD3", 
+        fontFamily: 'Aileron',
+        '&:hover': {
+          backgroundColor: '#db8ab4',
+          color: 'white',
+        },
+    };
 
     const headerStyle = {
         color: 'black',
@@ -37,11 +52,9 @@ const LandingPage = () => {
                 </div>
 
                 <div>
-                <Button variant="contained" size="large" defaultValue={30} sx={{width: 300,color: 'white', backgroundColor: "#F1CCD3", fontFamily: 'Aileron'}}> 
-                    Find out my hair type! 
-                </Button>
-
-
+                    <Button onClick={() => {navigate('/quiz'); }}variant="contained" size="large" defaultValue={30} sx={buttonStyle} > 
+                        Find out my hair type! 
+                    </Button>
                 </div>
                
             </Stack>
