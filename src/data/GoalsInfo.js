baabf6eda4955c20f86a10425f26887e46a94204
bfiskers <57647811+bfiskers@users.noreply.github.com>
2022-02-18 19:31:15 -0600

    4C health/growth recommendations added

--- src/data/GoalsInfo.js
+++ src/data/GoalsInfo.js
@@ -8,42 +8,42 @@ const tile_schema = {
 
 const goals_info_data = {
     Health: {
-        description: "Keep your hair healthy",
+        description: "From kinks to coils, 4C hair is versatile and can be shaped into almost any style. But it’s coil pattern leaves it susceptible to extreme dryness and frizziness. It’s important to keep those kinks and coils moisturized and ends trimmed regularly. Here are a few products to help you on your journey to healthy, moisture-rich hair.",
         tiles: [
             {
-                title: "Recommended Products",
-                moreInfo: "Check out some products we recommend to keep your hair healthy",
-                image: "hairProducts.png",
-                link: "https://www.sephora.com/beauty/best-selling-hair-products",
+                title: "BREAD BEAUTY SUPPLY Hair Oil Everyday Gloss",
+                moreInfo: "Goal: Keep hair looking shiny and healthy Recommendation: Choose a hair oil that keeps scalp and strands moisturized with a sticky oily residue",
+                image: "BREADBEAUTY.png",
+                link: "https://breadbeautysupply.com/",
             },
             {
-                title: "Magic Beans2",
-                moreInfo: "Magic beans help strengthen hair",
-                image: "magic_beans.jpeg",
-                link: "https://google.com",
+                title: "Oribe Cleansing Creme for Moisture Control",
+                moreInfo: "Goal: Keep hair strong and moisturized\n Recommendation: Choose a daily cleansing cream for the ultimate hydration.",
+                image: "Oribe.png",
+                link: "https://www.oribe.com/oribestorefront/oribe/en/-/Collections/Moisture-%26-Control/Cleansing-Cr%C3%A8me-for-Moisture-%26-Control/p/400256",
             },
             {
-                title: "Heat",
-                moreInfo: "Too much heat is bad for your hair",
-                image: null,
+                title: "TGIN Moisture Rich Sulfate Free Shampoo For Natural Hair with Amla Oil and Coconut Oil",
+                moreInfo: "Goal: Keep hair strong and moisturized Recommendation: Choose a Sulfate-Free Shampoo to gently cleanse the hair without stripping it of its natural oils",
+                image: "tgin.png",
                 link: null,         
-            },
-            {
-                title: "Positivity goes a long way",
-                moreInfo: "People that are optimistic tend to have healthier hair. Scientists think this is related to stress and cortisol levels",
-                image: null,
-                link: "https://google.com",
             }
         ]
     },
     Growth: {
-        description: "Help your hair grow",
+        description: "4C type hair is prone to breakage due to its delicate nature, so protective styles help to keep strands protected from split ends and damage. Here are a few easy protective styles that you can do at home or book through your local stylist/salon.",
         tiles: [
             {
-                title: "Deep Thought",
-                moreInfo: "Research shows that keeping your brain active helps hair growth",
-                image: "magic_beans.jpeg",
-                link: "https://www.researchgate.net"
+                title: "Knotless Box Braids",
+                moreInfo: "Eliminate breakage and protect your strands from root to tip with a low maintenance knotless box braid.",
+                image: "knotlessbraids.png",
+                link: null
+            },
+            {
+                title: "Low Bun",
+                moreInfo: "A quick and easy go to style to protect your ends. A low bun or a top bun can be a great style for any occasion and know that you are protecting your delicate strands in the process.",
+                image: "lowbun.png",
+                link: null
             }
         ]
     }
