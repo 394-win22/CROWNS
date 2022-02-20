988aced90bc8eec1fc0c2f8ad544667375571f35
James White <jameswhite2022@u.northwestern.edu>
2022-02-20 13:58:32 -0600

    UI updates

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,10 +2,12 @@ import React from "react";
 import Stack  from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Logo from '../images/CROWNSLOGO.png';
+//import Background from '../images/landing-page-background.jpg';
 import { useNavigate } from "react-router-dom";
 import "@fontsource/aileron";
 import "@fontsource/caveat";
 import "@fontsource/raleway";
+import { CoverageMap } from "istanbul-lib-coverage";
 
 const LandingPage = () => {
     console.log("navigated to landing page");
@@ -35,34 +37,41 @@ const LandingPage = () => {
     };
 
     const headerStyle = {
-        color: 'black',
+        color: 'white',
         fontSize: '3rem',
         fontWeight: 'bold',
         fontFamily: 'Raleway',
         paddingTop: '2rem',
+        paddingBottom: '20rem'
       };
 
     return (
         <div className="LandingPage" style={{
-            paddingBottom: "2rem"
+            paddingBottom: "2rem",
             // position: 'absolute', left: '50%', top: '45%',
             // transform: 'translate(-50%, -50%)'
+            backgroundImage: "url(./images/landing-page-background.jpg)",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            //height: "100%"
+            backgroundPosition: "center"
         }}>
             <Stack
                 direction="column"
                 justifyContent="center"
                 alignItems="center"
                 spacing={0}
+                height="100vh"
             >
                 <div style={headerStyle}> 
                    CROWNS
                 </div>
-                <div>
+                {/* <div>
                     <img style={{maxWidth: '20rem'}} src={Logo} alt=''/>
-                </div>
-                <div style={{fontFamily: 'Caveat', fontSize: '1.5rem'}}>
-                    Hey Girl! Are you ready to embrace your <br></br> hair and take back control? CROWNS <br></br>recommends products, hairstyles, 
-                    <br></br>and more to keep your hair healthy and strong!
+                </div> */}
+                <div style={{fontFamily: 'Caveat', fontSize: '1.5rem', color: 'white'}}>
+                    Are you ready to embrace your hair and take back control? Crowns recommends products, hairstyles, and stylists to guide you on your
+                    hair journey or strong healthy hair!
                 </div>
 
                 <div style={{margin: '2rem 0rem'}}>
