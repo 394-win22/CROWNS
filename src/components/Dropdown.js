1a0e0b3f509aa0a26df5c367c9a8ab50af198468
Brian Gleason <51419601+BrianGleason@users.noreply.github.com>
2022-02-24 14:35:55 -0600

    formatting stuff

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -6,55 +6,66 @@ import Typography from "@mui/material/Typography";
 import Grid from '@mui/material/Grid';
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { products_info_data } from "../data/ProductsInfo";
+import "@fontsource/raleway";
+import { makeStyles } from "@material-ui/core/styles";
 
-const Dropdown = ( {hairType}) => { 
+
+
+const Dropdown = ({ hairType }) => {
     const data = products_info_data[hairType];
+    const subsections = ["shampoos", "conditioners", "leaveIns"]
+    const formattedSections = {"shampoos": "Shampoos", "conditioners": "Conditioners", "leaveIns": "Leave-ins"}
 
-
+    const useStyles = makeStyles({
+    content: {
+        justifyContent: "center"
+    }});
+    const classes = useStyles();
 
     return (
         <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="products"
-            id="products"
-          >
-            <Typography sx={{ color: "#db8ab4", fontWeight: "bold" }}>
-              Products
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-                <Grid container>
-                    {data.shampoos.map((e, index) => {
-                        return (
-                            <Grid item xs={6} sm={6} md={4} key={index} sx={{ p: 1.5 }} >
-                                <img style={{ width: `${100}%`, height: 'auto' }} src={"./images/goals/" + e.image} alt={``} />
-                                <Typography sx={{ color: "black", fontWeight: "300", fontSize: "0.6rem"}}>
-                                    {e.title}
-                                </Typography>
-                                <Typography sx={{ color: "black", fontWeight: "bold", fontSize: "0.6rem" }}>
-                                    By: {e.brand}
-                                </Typography>
-                            </Grid>)
-                    })}
-                </Grid>
-                <Grid container spacing={{ xs: 0.5 }}>
-                    {data.conditioners.map((e, index) => {
-                        return (
-                            <Grid item xs={6} sm={6} md={4} key={index} >
-                                <img style={{ width: `${100}%`, height: 'auto' }} src={"./images/goals/" + e.image} alt={``} />
-                            </Grid>)
-                    })}
-                </Grid>
-                <Grid container spacing={{ xs: 0.5 }}>
-                    {data.leaveIns.map((e, index) => {
-                        return (
-                            <Grid item xs={6} sm={6} md={4} key={index} >
-                                <img style={{ width: `${100}%`, height: 'auto' }} src={"./images/goals/" + e.image} alt={``} />
-                            </Grid>)
-                    })}
-                </Grid>
-          </AccordionDetails>
+            <AccordionSummary
+                expandIcon={<ExpandMoreIcon sx={{color: "white"}}/>}
+                aria-controls="products"
+                id="products"
+                classes = {{content: classes.content}}
+                sx={{
+                    backgroundColor: "#B28181",
+                  }}
+            >
+                <Typography variant="h5" sx={{ color: "white", fontFamily: 'Raleway'}}>
+                    Products
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                {subsections.map((subsection) => {
+                    return (
+                        <>
+                        {data[subsection].length !== 0 && 
+                        <Typography variant="h5">
+                            {formattedSections[subsection]}
+                        </Typography>
+                        }
+
+                        <Grid container>
+                            {data[subsection].map((e, index) => {
+                                return (
+                                    <Grid item xs={6} sm={6} md={4} key={index} sx={{ p: 1.5 }} >
+                                        <img style={{ width: `${100}%`, height: 'auto' }} src={"./images/goals/" + e.image} alt={``} />
+                                        <Typography sx={{ color: "black", fontWeight: "300", fontSize: "0.6rem" }}>
+                                            {e.title}
+                                        </Typography>
+                                        <Typography sx={{ color: "black", fontWeight: "bold", fontSize: "0.6rem" }}>
+                                            By: {e.brand}
+                                        </Typography>
+                                    </Grid>)
+                            })}
+                        </Grid>
+                        </>
+                        
+                    )
+                })}
+            </AccordionDetails>
         </Accordion>
     )
 }
