996ae4f68631dc667b6a1f0d9698a6132a38c3a7
sethdmay <seth@teammay.com>
2022-02-27 14:43:43 -0600

    Remove navbar from quiz & landing

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ function App() {
     <div className="App">
       <Router>
             <Routes>
-                <Route exact path="/" element={<><LandingPage /><Navbar/></>} />
+                <Route exact path="/" element={<LandingPage />} />
                   <Route exact path="/results" element={<><ResultsPage hairType={hairType} /><Navbar/></>}/>
-                  <Route exact path="/quiz" element={<><HairQuiz setHairType={setHairType} /><Navbar /></>} />
+                  <Route exact path="/quiz" element={<HairQuiz setHairType={setHairType} />} />
                   <Route exact path="/selector" element={<><TypeSelector setHairType = {setHairType} /><Navbar /></>} />
                   <Route exact path="/profile" element={<><Profile /><Navbar/></>} />
                   <Route exact path="/testDropdown" element={<><ProductsDropdown hairType={"_4c"} /><Navbar /></>} />
