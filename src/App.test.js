8ac85ee45050247e6329005ae3f6671d8ca51020
bfiskers <57647811+bfiskers@users.noreply.github.com>
2022-03-07 16:07:10 -0600

    Fixed first test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders I know my type button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/I know my type!/i);
   expect(linkElement).toBeInTheDocument();
 });
