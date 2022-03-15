029b7c41aac02eb027b6e1e910cb657808c17664
Timxuv587 <1430415000@qq.com>
2022-03-15 17:25:31 -0500

    test firestore

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -18,7 +18,7 @@ const auth = getAuth(app);
 
 if (window.Cypress) {
     connectAuthEmulator(auth, "http://localhost:9099");
-    // connectFirestoreEmulator(db, "localhost", 8080);
+    connectFirestoreEmulator(db, "localhost", 8080);
 
     signInWithCredential(auth, GoogleAuthProvider.credential(
         '{"sub": "bcf1ucJn4ya8ou09q7uoNAUETKrL", "email": "qiuyangxu123@gmail.com", "displayName":"March 12 test", "email_verified": true}'
