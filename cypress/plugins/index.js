c2a01f812970c3f6e7bee866038d21017330b6c9
sethdmay <seth@teammay.com>
2022-03-15 14:47:47 -0500

    excluded default cypress tests

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -19,4 +19,6 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  config.ignoreTestFiles = ["**/integration/1-getting-started/*.spec.js","**/integration/2-advanced-examples/*.spec.js"];
+  return config;
 }
