e11d72f9528bf8919cf899358a8d3e5c05fd6ab1
Timxuv587 <1430415000@qq.com>
2022-03-15 17:20:57 -0500

    set up

---
+++ cypress/integration/TestGoalShowUp.spec.js
@@ -0,0 +1,36 @@
+/* globals cy */
+
+describe ('Test App', () => {
+
+    it ('launches', () => {
+      cy.visit ('/');
+    });
+
+    it ('opens with landing page', () => {
+        cy.visit ('/');
+        cy.get('[data-cy=title]').should('contain', 'CROWNS');
+      });
+
+    it ('clicks quiz button', () => {
+        cy.visit ('/');
+        
+        cy.get('[data-cy=quiz]').click();
+        cy.location().should((loc) => {
+            expect(loc.pathname).to.eq('/quiz')
+        });
+        cy.get('[data-cy=quizQuestion]').should('be.visible');
+    })
+    /*
+    it ('tries to sign in', () => {
+        cy.visit ('/');
+
+        cy.get('[data-cy=signIn').click();
+        
+    })
+
+    it('log in test', () => {
+        cy.login();
+    })
+    */
+  
+});
