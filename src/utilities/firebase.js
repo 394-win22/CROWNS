48b9acfa9294ad220a541a9fd9746674b38d3460
bfiskers <57647811+bfiskers@users.noreply.github.com>
2022-03-15 15:15:56 -0500

    Removed firebase info

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore, getDoc, doc, setDoc, updateDoc } from "firebase/firestore
 import { getAuth, GoogleAuthProvider, onIdTokenChanged, signInWithPopup, signOut, connectAuthEmulator, signInWithCredential } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB8Xulh0Uh7Jy2AHJVQOiBf4vTK2F2aotw",
-    authDomain: "crown-dd66d.firebaseapp.com",
-    projectId: "crown-dd66d",
-    storageBucket: "crown-dd66d.appspot.com",
-    messagingSenderId: "640118822953",
-    appId: "1:640118822953:web:6b9e6891d3c429d79ea438"
+    apiKey: "<Insert from firebase>",
+    authDomain: "<Insert from firebase>",
+    projectId: "<Insert from firebase>",
+    storageBucket: "<Insert from firebase>",
+    messagingSenderId: "<Insert from firebase>",
+    appId: "<Insert from firebase>"
 };
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 export const db = getFirestore(app);
