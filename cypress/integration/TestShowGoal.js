8541f7fadc0b50e3a3516b202552f95d1845fc32
Timxuv587 <1430415000@qq.com>
2022-03-15 21:33:52 -0500

    Qiuyang test goal not shown

--- cypress/integration/TestShowGoal.js
+++ cypress/integration/TestShowGoal.js
@@ -2,25 +2,6 @@
 
 describe ('Test App', () => {
 
-    it ('launches', () => {
-      cy.visit ('/');
-    });
-
-    it ('opens with landing page', () => {
-        cy.visit ('/');
-        cy.get('[data-cy=title]').should('contain', 'CROWNS');
-      });
-
-    it ('clicks quiz button', () => {
-        cy.visit ('/');
-        
-        cy.get('[data-cy=quiz]').click();
-        cy.location().should((loc) => {
-            expect(loc.pathname).to.eq('/quiz')
-        });
-        cy.get('[data-cy=quizQuestion]').should('be.visible');
-    })
-    
     it ('goal not shown', () => {
         cy.visit ('/');
 
